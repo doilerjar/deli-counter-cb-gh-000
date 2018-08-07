d5e15e46d63f34257c7cb8b059089af4f30de6ca
Kevin K <doilerjar@outlook.com>
2018-08-07 03:29:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,6 +9,8 @@ def line(deli)
   if line.size == 0 
     puts "The line is currently empty."
   else
-    
+    result = []
+    deli.each{|name| result << "#{deli.index(name)}. #{name}"}
+    return result.join(" ")
   end 
 end 
